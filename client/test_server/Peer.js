89831b7e2b95c96c09d5d49c73d50254e481a3e5
Henry <onkelhoy@gmail.com>
2019-01-08 13:55:34 +0100

    client commit

---
+++ client/test_server/Peer.js
@@ -0,0 +1,220 @@
+class Peer {
+  constructor (signalingServer, options = {}) {
+    this.socket = new window.WebSocket(signalingServer) 
+    this.peers = [] 
+    this.room = null 
+    this.id = null
+    this.level = 0
+    this.name_value = options.name || 'Sir Noname'
+    this.hooks = { 
+      socketSend: this.socketSend.bind(this),
+      newdatachannel: this.newdatachannel.bind(this),
+      datachannel: c => console.log('You have no handle function for datachannel', c)
+    }
+    this.dbNames = options.dataChannels || []
+    this.offerOptions = {}
+    this.localStream = null 
+    this.host = false 
+
+    this.servers = options.servers || {
+      iceServers: [{urls: "stun:stun.l.google.com:19302"}]
+    }
+    this.onSocketMessage = this.onSocketMessage.bind(this)
+    this.socket.onmessage = this.onSocketMessage
+  }
+
+  set name (value) {
+    this.name_value = value 
+    this.socketSend({ type: 'name change', name: value })
+  }
+  get name () {
+    return this.name_value
+  }
+
+  on (hook, cb) {
+    this.hooks[hook.toLowerCase()] = cb
+  }
+  onSocketMessage (message) {
+    let data = JSON.parse(message.data)
+    switch (data.type) {
+      case 'connected':
+        this.id = data.id 
+        break;
+      case 'room disconnect':
+        this.room = null 
+        // remove all rtc clients 
+        this.peers = []
+        break;
+      case 'disconnect':
+        console.log('player disconnected', data.from)
+        for (let i = 0; i < this.peers.length; i++)
+          if (this.peers[i].id === data.from) 
+            return this.peers.splice(i, 1)
+        break;
+      case 'new client':
+        console.log('new client connected')
+        this.connectWith(data.id, data.name, data.level)
+        break;
+      case 'message':
+        console.log(data.message, this.hooks.message)
+        if (this.hooks.message) this.hooks.message(data.message)
+        break;
+      case 'room connect':
+        this.peers = []
+        this.room = data.room
+        this.host = data.host
+        this.level = data.level
+        if (this.hooks.connect) this.hooks.connect(data.host)
+        break;
+      case 'offer':
+        if (data.to === this.id)
+          this.receiveOffer(data)
+        break;
+      case 'answer':
+        if (data.to === this.id)
+          this.receiveAnswer(data)
+        break;
+      case 'candidate':
+        if (data.to === this.id)
+          this.receiveCandidate(data)
+        break;
+      case 'host':
+        this.host = true 
+        break;
+      case 'name change':
+        for (let peer of this.peers) 
+          if (data.from === peer.id) peer.name = data.name
+        break;
+      case 'error':
+        if (this.hooks.error) this.hooks.error(data.error)
+        break;
+    } 
+
+    // if user hooks own onmessage events 
+  }
+  socketSend (data) {
+    this.socket.send(JSON.stringify(data))
+  } 
+  async newdatachannel (label, from_id) {
+    // add same channel to rest of peers (but only if their rank is higher)
+    let exists = false 
+    for (let { name } of this.dbNames) {
+      if (name === label) exists = true  
+    }
+    if (!exists) this.dbNames.push({ name: label, config: {} }) 
+
+    for (let peer of this.peers) {
+      if (from_id !== peer.id && peer.level > this.level) {
+        if (!peer.markedChannels[label]) { // .find(label => label === event.channel.label)) {
+          peer.addDataChannel(label, this.hooks.datachannel, {})
+
+          try {
+            let offer = await peer.createOffer()
+            this.socketSend({ type: 'offer', to: peer.id, offer, name: this.name })
+          } catch (e) {
+            this.hooks.error(e)
+          }
+        }
+      }
+    }
+  }
+  
+  //* happens : when someone connects to room (new)
+  //* as this client connects to other, this is then HOST
+  async connectWith (id, name, level) {
+    let peer = new RTCPeer(id, name, this.servers, this.hooks, level)
+    this.peers.push(peer)
+
+    // only make offer 
+    for (let {name, config} of this.dbNames) 
+      peer.addDataChannel(name, this.hooks.datachannel, config)
+    
+    try {
+      let offer = await peer.createOffer()
+      this.socketSend({ type: 'offer', to: peer.id, offer, name: this.name, level: this.level })
+    } catch (e) {
+      this.hooks.error(e)
+    }
+  } 
+  async receiveOffer (data) { // has offer and from who (id)
+    // let connection = this.createPeer(data.from, data.name)
+    // create peer if not created 
+    let peer = undefined
+    for (let peer2 of this.peers) 
+      if (peer2.id === data.from) peer = peer2 
+    
+    // only created if not exist
+    if (!peer) {
+      peer = new RTCPeer(data.from, data.name, this.servers, this.hooks, data.level)
+      this.peers.push(peer)
+    }
+    
+    try {
+      let answer = await peer.receiveOffer(data.offer)
+      this.socketSend({ type: 'answer', to: data.from, answer })
+    }
+    catch (e) {
+      this.hooks.error(e)
+    }
+  }
+  receiveAnswer (data) {
+    for (let peer of this.peers) {
+      if (peer.id === data.from) {
+        try {
+          peer.receiveAnswer(data.answer)
+        }
+        catch (e) {
+          this.hooks.error(e)
+        }
+      }
+    }
+  } 
+  receiveCandidate (data) {
+    for (let peer of this.peers) {
+      if (peer.id === data.from) {
+        try {
+          peer.receiveCandidate(data.candidate)
+        } catch (e) {
+          this.hooks.error(e)
+        }
+      }
+    }
+  }
+
+  //* Manual functions to be made from user
+  create (roomName, options = { password: undefined, max: 10 }) {
+    this.socketSend({ type: 'create', room: roomName, password: options.password, max: options.max })
+  }
+  join (roomID, password) {
+    this.socketSend({ type: 'join', room: roomID, password, name: this.name })
+  }
+  addDataChannel (name, config = {}) {
+    for (let peer of this.peers) 
+      peer.addDataChannel(name, this.hooks.datachannel, config)
+    
+    // try creating a new offer! 
+    this.dbNames.push({name, config})
+  }
+  async addStream (option = {video:true, audio:true}) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      console.log('received stream')
+      this.offerOptions = {
+        offerToReceiveAudio: options.audio,
+        offerToReceiveVideo: options.video,
+      }
+      this.localStream = stream
+    }
+    catch (e) {
+      console.error('get user media error: ' + e.name)
+      if (this.hooks.error) this.hooks.error(e)
+    }
+  }
+  send (name, data) {
+    if (data instanceof Object)
+      data = JSON.stringify(data)
+
+    for (let peer of this.peers) 
+      peer.send(name, data)
+  }
+}
---
+++ client/test_server/RTCPeer.js
@@ -0,0 +1,78 @@
+class RTCPeer {
+  constructor (id, name, servers, hooks, level) {
+    this.id = id 
+    this.name = name 
+    this.markedChannels = [] 
+    this.channels = [] 
+    this.level = level
+    this.rtc = new window.RTCPeerConnection(servers)
+    this.cr = false 
+
+    this.initRTC(hooks)
+  }
+
+  initRTC (hooks) {
+    this.rtc.ondatachannel  = event => {
+      this.configChannel(event.channel, hooks.datachannel)
+      hooks.newdatachannel(event.channel.label, this.id)
+    }
+    this.rtc.onicecandidate = event => {
+      hooks.socketSend({ type: 'candidate', to: this.id, candidate: event.candidate })
+    } 
+    this.rtc.ontrack = null 
+  }
+
+  async receiveOffer (offer) {
+    await this.rtc.setRemoteDescription(new RTCSessionDescription(offer))
+    let answer = await this.rtc.createAnswer() // create answer
+    await this.rtc.setLocalDescription(answer)
+    return await this.rtc.localDescription // send it via socket (take care of errors)
+  }
+  async receiveAnswer (answer) {
+    await this.rtc.setRemoteDescription(new RTCSessionDescription(answer))
+  }
+  async receiveCandidate (candidate) {
+    if (!this.cr) {
+      this.cr = true 
+      await this.rtc.addIceCandidate(new RTCIceCandidate(candidate))
+    }
+  }
+
+  async createOffer () {
+    let offer = await this.rtc.createOffer()
+    await this.rtc.setLocalDescription(offer)
+
+    return this.rtc.localDescription // send it via socket (take care of errors)
+  }
+  
+  configChannel (channel, hook) {
+    this.channels.push(channel)
+    this.markedChannels[channel.label] = true 
+
+    if (hook) {
+      hook(channel)
+      // we want to modify the channel on message function 
+      let raw = channel.onmessage // storing the raw function set by user 
+      channel.onmessage = message => {
+        let data = message.data
+        try {
+          let data = JSON.parse(data)
+        } 
+        catch (e) {} // leave it.. its only text
+        raw(data, { id: this.id, name: this.name })
+      }
+    }
+  } 
+  addDataChannel (name, hook, config = {}) {
+    if (this.markedChannels[name] === undefined) {
+      let channel = this.rtc.createDataChannel(name, config)
+      this.configChannel(channel, hook)
+    }
+  }
+
+
+  send (name, data) {
+    for (let channel of this.channels) 
+      if (channel.label === name) channel.send(data)
+  }
+}
